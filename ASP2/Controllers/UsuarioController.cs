c683c62d801f66f0c402e2f5e2924bd1e9b91a57
Brayan Perdomo <brayanbushi05@gmail.com>
2021-05-10 14:09:15 -0500

    Agregar archivos de proyecto.

---
+++ ASP2/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2.Models;
+
+namespace ASP2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.usuario.ToList());
+
+            }
+            
+        }
+    }
+}
